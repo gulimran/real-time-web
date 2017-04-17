2e710417f3d82537205aa8faf42b8fb2f3e61168
Gul Imran <gimran.it@gmail.com>
2017-04-17 17:50:47 +0100

    Using express

--- airline/routes/index.js
+++ airline/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var flight = require('../flight');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// flight objects
+
+var flight1 = flight({
+  number       : 1,
+  origin       : 'LAX',
+  destination  : 'DCA',
+  departs      : '9AM',
+  arrives      : '4PM'
+});
+
+var flight2 = flight({
+  number       : 1,
+  origin       : 'LAX',
+  destination  : 'PDX',
+  departs      : '10AM',
+  arrives      : '12PM'
+});
+
+router.get('/flight1', function(req, res, next) {
+  res.json(flight1.getInformation());
+});
+
+router.get('/flight2', function(req, res, next) {
+  res.json(flight2.getInformation());
 });
 
 module.exports = router;
