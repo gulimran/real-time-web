5d8b355a0b394b92ccdd74a942974e8e28f1f13f
Gul Imran <gimran.it@gmail.com>
2017-04-22 15:50:32 +0100

    Express app with rest APIs and header customizaion

---
+++ flights-rest-apis/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (req, res, next) {
+    res.set('X-Powered-By', 'Flight tracker');
+    next();
+});
+
+app.use('/', index);
+
+
+module.exports = app;
---
+++ flights-rest-apis/routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var flight = require('../flight');
+var flights = require('../data');
+
+for (var number in flights) {
+    flight[number] = flight(flights[number]);
+}
+
+router.get('/flight/:number', function(req, res, next) {
+    var number = req.param('number');
+
+    if (typeof flights[number] === 'undefined') {
+        res.status(404).json({status: 'error'});
+    } else {
+        res.json(flight[number].getInformation());
+    }
+});
+
+router.put('/flight/:number/arrived', function(req, res, next) {
+    var number = req.param('number');
+
+    if (typeof flights[number] === 'undefined') {
+        res.status(404).json({status: 'error'});
+    } else {
+        flight[number].triggerArrive();
+        res.json({status: 'arrived'});
+    }
+});
+
+module.exports = router;
