45492bb555b13295aa8c2399bbc7805c77e8da8f
Gul Imran <gimran.it@gmail.com>
2017-04-22 18:43:05 +0100

    Decouple server code from app

---
+++ flights-decouple-server/routes/index.js
@@ -0,0 +1,38 @@
+module.exports = function (flights) {
+
+    var express = require('express');
+    var router  = express.Router();
+    var flight  = require('../flight');
+
+    for (var number in flights) {
+        flight[number] = flight(flights[number]);
+    }
+
+    router.get('/flight/:number', function(req, res, next) {
+        var number = req.param('number');
+
+        if (typeof flights[number] === 'undefined') {
+            res.status(404).json({status: 'error'});
+        } else {
+            res.json(flight[number].getInformation());
+        }
+    });
+
+    router.put('/flight/:number/arrived', function(req, res, next) {
+        var number = req.param('number');
+
+        if (typeof flights[number] === 'undefined') {
+            res.status(404).json({status: 'error'});
+        } else {
+            flight[number].triggerArrive();
+            res.json({status: 'arrived'});
+        }
+    });
+
+    router.get('/list', function(req, res, next) {
+        res.render('list', {title: "All Flights", flights: flights});
+    });
+
+    return router;
+
+}
