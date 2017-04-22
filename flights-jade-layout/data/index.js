7f305632c8bca4bfc51d95432f645777d5f16066
Gul Imran <gimran.it@gmail.com>
2017-04-22 17:56:31 +0100

    Using jade layout with express

---
+++ flights-jade-layout/data/index.js
@@ -0,0 +1,44 @@
+module.exports = {
+    "1" : {
+        "number"      : 1,
+        "origin"      : "KHI",
+        "destination" : "LON",
+        "departs"     : "10:00AM",
+        "arrives"     : "12:00PM"
+    },
+    "2" : {
+        "number"      : 2,
+        "origin"      : "LAX",
+        "destination" : "LON",
+        "departs"     : "6:00AM",
+        "arrives"     : "19:00PM"
+    },
+    "3" : {
+        "number"      : 3,
+        "origin"      : "BHX",
+        "destination" : "LON",
+        "departs"     : "11:00AM",
+        "arrives"     : "11:30AM"
+    },
+    "4" : {
+        "number"      : 4,
+        "origin"      : "YYZ",
+        "destination" : "LON",
+        "departs"     : "7:00AM",
+        "arrives"     : "17:00PM"
+    },
+    "11" : {
+        "number"      : 11,
+        "origin"      : "NYC",
+        "destination" : "LON",
+        "departs"     : "9:00AM",
+        "arrives"     : "13:30PM"
+    },
+    "24" : {
+        "number"      : 24,
+        "origin"      : "MCO",
+        "destination" : "LON",
+        "departs"     : "7:00AM",
+        "arrives"     : "22:00PM"
+    }
+}
